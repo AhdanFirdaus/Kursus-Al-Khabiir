0580e2b3584236e0672dc1ec75556162d44e3b49
AhdanFirdaus <muhammadahdanf1@gmail.com>
2023-12-27 13:29:31 +0700

    memeperbaiki file js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menu = document.querySelector(".menu");
-    const menuToggle = document.querySelector(".menu-toggle");
-
-    menuToggle.addEventListener("click", function () {
-        menu.classList.toggle("active");
-    });
-});
